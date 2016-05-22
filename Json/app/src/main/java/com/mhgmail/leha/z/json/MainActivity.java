7bd63fe5684bfe44b2f7b8a7b49b1ed07efac6c3
Zahar4506 <Z.leha.mh@gmail.com>
2016-05-23 00:10:57 +0500

    рабочий запрос

--- Json/app/src/main/java/com/mhgmail/leha/z/json/MainActivity.java
+++ Json/app/src/main/java/com/mhgmail/leha/z/json/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mhgmail.leha.z.json;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -10,10 +11,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 
 
 public class MainActivity extends ActionBarActivity {
@@ -27,21 +32,51 @@ public class MainActivity extends ActionBarActivity {
         new ParseTask().execute();
     }
 
+
+
     private class ParseTask extends AsyncTask<Void, Void, String> {
 
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
         String resultJson = "";
 
+
+
         @Override
         protected String doInBackground(Void... params) {
             // получаем данные с внешнего ресурса
             try {
-                URL url = new URL("http://mob.ugrasu.ru/oracle/database_tmtb_json_derji.php");
+               // URL url = new URL("http://mob.ugrasu.ru/oracle/database_tmtb_json_derji.php");
+                URL url = new URL("http://mob.ugrasu.ru/oracle/database_timetable.php");
 
                 urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
+              //  urlConnection.setRequestMethod("GET");
+                        urlConnection.setReadTimeout(15000);
+                        urlConnection.setConnectTimeout(15000);
+                    urlConnection.setRequestMethod("POST");
+
+                         urlConnection.setDoInput(true);
+                         urlConnection.setDoOutput(true);
+
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("timetable_query", "1541б")
+                        .appendQueryParameter("date_query", "24.05.2016");
+                String query = builder.build().getEncodedQuery();
+
+
+                OutputStream os = urlConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+
+                Log.d(LOG_TAG, query);
+
                 urlConnection.connect();
+
+
 
                 InputStream inputStream = urlConnection.getInputStream();
                 StringBuffer buffer = new StringBuffer();
@@ -80,7 +115,7 @@ public class MainActivity extends ActionBarActivity {
 
             try {
                 dataJsonObj = new JSONObject(strJson);
-                JSONArray dateUgrasu = dataJsonObj.getJSONArray("22032016");
+                JSONArray dateUgrasu = dataJsonObj.getJSONArray("24052016");
 
                 // 2. перебираем и выводим контакты каждого друга
                 for (int i = 0; i < dateUgrasu.length(); i++) {
@@ -103,5 +138,7 @@ public class MainActivity extends ActionBarActivity {
                 e.printStackTrace();
             }
         }
+
+
     }
 }
